d2219785b35787d12751e1f0dfd0f8ed50f79d3a
numdinkushi <numdinkushi@gmail.com>
2023-08-23 22:00:12 +0100

    fetch and display listings to home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,9 +31,12 @@ export default async function RootLayout({
           <RentModal />
           <LoginModal />
           <RegisterModal />
-          <Navbar currentUser = {currentUser} />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
-        {children}</body>
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,41 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import ClientOnly from './components/ClientOnly';
+import Container from './components/Container';
+import EmptyState from './components/EmptyState';
+import getListings from './actions/getListings';
+import ListingCard from './components/listings/ListingCard';
+import getCurrentUser from './actions/getCurrentUser';
 
-export default function Home() {
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+
+  if (listings?.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
   return (
-  <div className="">
-    hello kus
-  </div>
-  )
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+            {
+              listings.map((listing: any, index) => {
+                return (
+                  <ListingCard
+                    currentUser={currentUser}
+                    key={index}
+                    data={listing}
+                  />
+                );
+              })
+            }
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
