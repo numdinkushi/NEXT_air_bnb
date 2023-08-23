6cf1246c68ad18824fdcbad43f32c59dbb81dd93
numdinkushi <numdinkushi@gmail.com>
2023-08-23 13:41:46 +0100

    can create listing

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -34,7 +34,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
     >
       {({ open }) => {
         return (
-          <div onClick={() => open?.()} className=" relative cursor-pointer hover:opacity-70 transition border-dashed border-2 p-20 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600 " >
+          <div onClick={() => open && open?.()} className=" relative cursor-pointer hover:opacity-70 transition border-dashed border-2 p-20 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600 " >
             <TbPhotoPlus
               size={50}
             />
