800d6857a638eb3267d0c74bb9afc29a2190a075
numdinkushi <numdinkushi@gmail.com>
2023-08-24 12:56:36 +0100

    wip: single listing

---
+++ app/components/listings/ListingInfo.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import useCountries from "@/app/hooks/useCountries";
+import { SafeUser } from "@/app/types";
+import { IconType } from "react-icons";
+import Avatar from "../Avatar";
+
+interface ListingInfoProps {
+    user: SafeUser;
+    description: string;
+    guestCount: number;
+    bathroomCount: number;
+    roomCount: number;
+    category: {
+        icon: IconType,
+        label: string;
+        description: string;
+    } | undefined;
+    locationValue: string;
+
+}
+
+const ListingInfo: React.FC<ListingInfoProps> = ({
+    user,
+    description,
+    guestCount,
+    roomCount,
+    bathroomCount,
+    category,
+    locationValue
+}) => {
+    const { getByValue } = useCountries();
+
+    const coordinates = getByValue(locationValue)?.latlng;
+
+    return (
+        <div className="col-span-4 flex flex-col gap-8 ">
+            <div className="flex flex-col gap-2">
+                <div className="text-xl font-semibold flex flex-row items-center gap-2">
+                    <div className="">Hosted by {user?.name}</div>
+                    <Avatar />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ListingInfo;
---
+++ app/listings/[listingId]/ListingClient.tsx
@@ -0,0 +1,113 @@
+'use client';
+
+import axios from "axios";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { toast } from "react-hot-toast";
+// import { Range } from "react-date-range";
+import { useRouter } from "next/navigation";
+import { differenceInDays, eachDayOfInterval } from 'date-fns';
+
+import useLoginModal from "@/app/hooks/useLoginModal";
+import { SafeUser } from "@/app/types";
+
+import Container from "@/app/components/Container";
+import { categories } from "@/app/components/navbar/Categories";
+import { Listing, Reservation } from "@prisma/client";
+import ListingHead from "@/app/components/listings/ListingHead";
+import ListingInfo from "@/app/components/listings/ListingInfo";
+// import ListingHead from "@/app/components/listings/ListingHead";
+// import ListingInfo from "@/app/components/listings/ListingInfo";
+// import ListingReservation from "@/app/components/listings/ListingReservation";
+
+const initialDateRange = {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection'
+};
+
+interface ListingClientProps {
+    reservations?: Reservation[];
+    listing: Listing & {
+        user: SafeUser;
+    };
+    currentUser?: SafeUser | null;
+}
+
+const ListingClient: React.FC<ListingClientProps> = ({
+    listing,
+    reservations = [],
+    currentUser
+}) => {
+    const loginModal = useLoginModal();
+    const router = useRouter();
+
+    const disabledDates = useMemo(() => {
+        let dates: Date[] = [];
+
+        reservations.forEach((reservation: any) => {
+            const range = eachDayOfInterval({
+                start: new Date(reservation.startDate),
+                end: new Date(reservation.endDate)
+            });
+
+            dates = [...dates, ...range];
+        });
+
+        return dates;
+    }, [reservations]);
+
+    const category = useMemo(() => {
+        return categories.find((items) =>
+            items.label === listing.category);
+    }, [listing.category]);
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [totalPrice, setTotalPrice] = useState(listing.price);
+    //   const [dateRange, setDateRange] = useState<Range>(initialDateRange);
+
+    const onCreateReservation = useCallback(() => {
+        if (!currentUser) {
+            return loginModal.onOpen();
+        }
+        setIsLoading(true);
+
+    },
+        [
+            totalPrice,
+            // dateRange, 
+            listing?.id,
+            router,
+            currentUser,
+            loginModal
+        ]);
+
+
+    return (
+        <Container>
+            <div className=" max-w-screen-lg mx-auto " >
+                <div className="flex flex-col gap-6">
+                    <ListingHead 
+                        title={listing.title}
+                        imageSrc={listing.imageSrc}
+                        locationValue={listing.locationValue}
+                        id={listing.id}
+                        currentUser={currentUser}
+                    />
+                    <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+                        <ListingInfo 
+                            user = {listing.user}
+                            category={category}
+                            description={listing.description}
+                            roomCount={listing.roomCount}
+                            guestCount={listing.guestCount}
+                            bathroomCount={listing.bathroomCount}
+                            locationValue={listing.locationValue}
+                        />
+                    </div>
+                </div>
+            </div>
+        </Container>
+    );
+};
+
+export default ListingClient;
