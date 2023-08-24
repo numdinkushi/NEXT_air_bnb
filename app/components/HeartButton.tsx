304f81296921c1b6692b7cbf1764594053fc762e
numdinkushi <numdinkushi@gmail.com>
2023-08-24 11:00:48 +0100

    can add favourite listings

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -2,6 +2,7 @@
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
+import useFavorite from "../hooks/useFavorite";
 
 interface HeartButtonProps {
     listingId: string;
@@ -12,24 +13,27 @@ const HeartButton: React.FC<HeartButtonProps> = ({
     listingId,
     currentUser
 }) => {
-    const hasFavorited = false;
-    const toggleFavorite = () => {};
+   
+    const { hasFavorited, toggleFavorite } = useFavorite({
+        listingId,
+        currentUser
+      });    
 
-  return (
-    <div onClick={toggleFavorite} className="relative hover:opacity-80 transition cursor-pointer">
-        <AiOutlineHeart 
-        size={28}
-        className="fill-white absolute -top-[2px] -right-[2px]"
-        />
+    return (
+        <div onClick={toggleFavorite} className="relative hover:opacity-80 transition cursor-pointer">
+            <AiOutlineHeart
+                size={28}
+                className="fill-white absolute -top-[2px] -right-[2px]"
+            />
 
-        <AiFillHeart
-            size={24}
-            className={
-                hasFavorited ? 'fill-rose-500' : 'fill-neutral-500/70'
-            }
-        />
-    </div>
-  )
-}
+            <AiFillHeart
+                size={24}
+                className={
+                    hasFavorited ? 'fill-rose-500' : 'fill-neutral-500/70'
+                }
+            />
+        </div>
+    );
+};
 
-export default HeartButton
+export default HeartButton;
--- app/types/index.ts
+++ app/types/index.ts
@@ -2,11 +2,10 @@ import {User} from "@prisma/client";
 
 
 export type SafeUser = Omit<
-    UserActivation,
-    'createdAt' | 'updatedAt' | 'emailVerified'
+  User,
+  "createdAt" | "updatedAt" | "emailVerified"
 > & {
-    createdAt: string;
-    updatedAt: string;
-    emailVerified: string | null;
-    image?: string 
-}
+  createdAt: string;
+  updatedAt: string;
+  emailVerified: string | null;
+};
